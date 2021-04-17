86d2767672e1690506f85ced8e4cce9fb588668e
kbthapa-neb <kthapa@my365.bellevue.edu>
2021-04-17 14:22:29 -0500

    init

--- pytech_insert12.py
+++ pytech_insert12.py
@@ -4,12 +4,6 @@ from pymongo import MongoClient
 # MongoDB connection string 
 url = "mongodb+srv://admin:admin@cluster0.rsnru.mongodb.net/pytech?retryWrites=true&w=majority"
 
-# connect to the MongoDB cluster 
-client = MongoClient(url)
-
-# connect pytech database
-db = client.pytech
-
 """ three student documents"""
 # Thorin Oakenshield's data document 
 thorin = {
